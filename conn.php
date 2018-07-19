fd996164daf26a674246903aa2b80a640ad3d3b8
Waleed Sohail <wsohail123@gmail.com>
2018-07-19 22:52:18 +0500

    OnlineDB

--- conn.php
+++ conn.php
@@ -1,4 +1,4 @@
 <?php
-    $mysqli = new mysqli("localhost", "root", "", "PharmaX");
+    $mysqli = new mysqli("mi3-ss31.a2hosting.com:3306", "vleresea_pharmax", "PharmaX-PBS", "vleresea_pharmax");
     $result = $mysqli->query("SELECT * FROM login");
 ?>
